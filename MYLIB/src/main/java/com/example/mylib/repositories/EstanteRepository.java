f103fb179063468bd02c53b6b538ca1c6f57ba20
Frederyck_Lohan <lohangcfc@gmail.com>
2025-09-17 21:45:12 -0300

    remoção
    remoção de entidade e seus serviços

--- MYLIB/src/main/java/com/example/mylib/repositories/EstanteRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.mylib.repositories;
-
-import com.example.mylib.models.Estante;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EstanteRepository extends JpaRepository<Estante, Long> {
-}
