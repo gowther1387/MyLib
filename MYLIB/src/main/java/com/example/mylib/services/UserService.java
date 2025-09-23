743c9dd46311e71a0094e9562d0f377373d175c4
Frederyck_Lohan <lohangcfc@gmail.com>
2025-09-22 21:34:23 -0300

    att

--- MYLIB/src/main/java/com/example/mylib/services/UserService.java
+++ MYLIB/src/main/java/com/example/mylib/services/UserService.java
@@ -1,8 +1,12 @@
 package com.example.mylib.services;
 
 
+import com.example.mylib.models.User;
 import com.example.mylib.repositories.UserRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
 
 @Service
 public class UserService {
@@ -13,6 +17,22 @@ public UserService(UserRepository userRepository) {
     this.userRepository = userRepository;
 }
 
+public User postUser(User user){
+    return userRepository.save(user);
+}
+
+
+public User getUser(UUID id){
+    return userRepository.getUsersById(id);
+}
+
+public List<User> getUsers(){
+    return userRepository.findAll();
+}
+
+public void deleteUser(UUID id){
+    userRepository.deleteById(id);
+}
 
 
 }
